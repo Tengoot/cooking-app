86cfe9f81044f70c4bb67381fb43e68f69d08fb1
Bartosz Kowalski <b.kowalski@visuality.pl>
2019-11-01 20:41:41 +0100

    Updates

---
+++ precache-manifest.6e9fa5c1c3c1ae70d0675c52aeb414f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c1822b3052122efc8c275d5fea97e5",
+    "url": "/cooking-app/index.html"
+  },
+  {
+    "revision": "6751b0d611590c8d8878",
+    "url": "/cooking-app/static/css/main.d624beac.chunk.css"
+  },
+  {
+    "revision": "ad3fa2ac38f2823e9aeb",
+    "url": "/cooking-app/static/js/2.bd2380db.chunk.js"
+  },
+  {
+    "revision": "6751b0d611590c8d8878",
+    "url": "/cooking-app/static/js/main.005e984c.chunk.js"
+  },
+  {
+    "revision": "913bad5311e8494d3df7",
+    "url": "/cooking-app/static/js/runtime-main.bf8472c6.js"
+  },
+  {
+    "revision": "bd26dc32cfa3d85af97f24988393ee67",
+    "url": "/cooking-app/static/media/logo.bd26dc32.svg"
+  }
+]);
