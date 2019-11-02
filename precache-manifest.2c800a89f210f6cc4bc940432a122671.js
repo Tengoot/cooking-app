87d2ed052f4c7c28390a93f9d93fd59fde533a68
Bartosz Kowalski <b.kowalski@visuality.pl>
2019-11-02 08:38:47 +0100

    Updates

---
+++ precache-manifest.2c800a89f210f6cc4bc940432a122671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d10aae7d8253bb7c8b8de2ac9262a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e8e651b3bcb2e56b70",
+    "url": "/static/css/main.d624beac.chunk.css"
+  },
+  {
+    "revision": "ad3fa2ac38f2823e9aeb",
+    "url": "/static/js/2.bd2380db.chunk.js"
+  },
+  {
+    "revision": "98e8e651b3bcb2e56b70",
+    "url": "/static/js/main.754e687d.chunk.js"
+  },
+  {
+    "revision": "ad2934e07115c2c0cf0e",
+    "url": "/static/js/runtime-main.45a201ff.js"
+  },
+  {
+    "revision": "bd26dc32cfa3d85af97f24988393ee67",
+    "url": "/static/media/logo.bd26dc32.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cooking-app/precache-manifest.6e9fa5c1c3c1ae70d0675c52aeb414f5.js"
+  "/precache-manifest.2c800a89f210f6cc4bc940432a122671.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cooking-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
